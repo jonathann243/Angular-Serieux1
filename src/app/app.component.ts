b631b7b1ff546dbb5991591c20226d2fe210dca7
JONATHAN K <55114686+jonathann243@users.noreply.github.com>
2022-11-04 21:03:07 -0400

    Component add new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+// decorateur for telling complilator  this is not plane class this is conponet
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  // tag html representing this component (use in index html)
+  templateUrl: './app.component.html',//pointe html file
+  styleUrls: ['./app.component.css']// point css file
 })
 export class AppComponent {
-  title = 'Titre de la questions';
+  title = 'Titre de la questisvsvsfuons';
+  id=45;
+  proof =45;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
+  // all componet use by application
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
