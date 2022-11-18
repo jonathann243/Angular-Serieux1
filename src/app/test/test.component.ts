7127836af670be142dc7bfec24af7a1944257cfd
JONATHAN K <55114686+jonathann243@users.noreply.github.com>
2022-11-18 01:48:45 -0500

    ng switch

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  displayState=true;
+  color="bluee";
 
   constructor() { }
     ngOnInit(): void {
